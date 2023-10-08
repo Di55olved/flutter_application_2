4b8ec3059a17df1ffa680633eb8d57acba21c255
Di55olved <b.siddiqui_19761@khi.iba.edu.pk>
2023-10-09 01:22:20 +0500

    Day 2 Completed

---
+++ lib/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    String name = "burhan";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Text("Welcome to $days days flutter by $name"),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
